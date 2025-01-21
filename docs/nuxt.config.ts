b5d1a641a311c296cc2d2c2c199bdd73f4b0f76c
Felipe Oliveira <felipe16.12.1998@gmail.com>
2025-01-21 10:18:24 -0300

    chore(docs): add paper module

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@nuxt/content',
+    '@paper-ui/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/content'
   ],
   devtools: { enabled: true },
   future: {
