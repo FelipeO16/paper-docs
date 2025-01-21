d8815f1610100012885b9f9e57d79291eae86c4e
Felipe Oliveira <felipe16.12.1998@gmail.com>
2025-01-21 09:20:41 -0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  modules: [
+    '@paper-ui/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/content'
+  ],
+  devtools: { enabled: true }
+})
