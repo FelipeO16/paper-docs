1ba93a49822f5ad89eddae6d6acb02aa64511f51
Felipe <felipe16.12.1998@gmail.com>
2025-01-23 12:38:18 -0300

    chore(docs): add content options and debug

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,14 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
+    '@nuxt/content',
     '@paper-ui/nuxt',
-    '@nuxtjs/tailwindcss',
-    '@nuxt/content'
+    '@nuxtjs/tailwindcss'
   ],
   ssr: false,
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
   },
+  content: {
+    preview: {
+      api: 'https://api.nuxt.studio'
+    },
+    build: {
+      markdown: {
+        highlight: {
+          theme: {
+            default: 'github-dark',
+          }
+        }
+      }
+    }
+  },
+  nitro: {
+    prerender: {
+      failOnError: false, // Não interrompe o build em erros de prerender
+    },
+  },
+  debug: true,
   compatibilityDate: '2024-04-03',
 })
