dd245f5a3c437a3fbe40994b6cf287506969ba1a
Felipe <felipe16.12.1998@gmail.com>
2025-01-23 08:51:49 -0300

    chore(ci): add dist folder to build

---
+++ dist/_nuxt/vAnFFfFs.js
@@ -0,0 +1,1 @@
+import{_ as o,o as c,c as i,a as s,N as e}from"./G58y70Rf.js";const a={},n={class:"my-4 space-y-2"},r={class:"topic-title text-xl font-bold"},l={class:"topic-description text-lg"};function d(t,_){return c(),i("div",n,[s("div",r,[e(t.$slots,"title")]),s("div",l,[e(t.$slots,"description")])])}const f=o(a,[["render",d]]);export{f as default};
